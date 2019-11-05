30a156958adceb791a129d0832d03bc7f3c48ade
aglegaspi <lex83nyc@gmail.com>
2019-11-05 11:43:39 -0500

    set up app for programmatic coding

---
+++ BellyFund/LoginVC.swift
@@ -0,0 +1,20 @@
+//
+//  LoginVC.swift
+//  BellyFund
+//
+//  Created by Alex 6.1 on 11/5/19.
+//  Copyright © 2019 aglegaspi. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+    
+    //MARK: LIFECYCLES
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+        // Do any additional setup after loading the view.
+    }
+
+}
--- Podfile
+++ Podfile
@@ -12,6 +12,8 @@ pod 'Firebase/Analytics'
 # add pods for any other desired Firebase products
 # https://firebase.google.com/docs/ios/setup#available-pods
 
+pod 'Firebase/Auth'
+
   target 'BellyFundTests' do
     inherit! :search_paths
     # Pods for testing
