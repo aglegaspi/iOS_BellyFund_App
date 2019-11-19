6a14f8b3518b2f9892a37d3673eeff1b9b8cb2ad
aglegaspi <lex83nyc@gmail.com>
2019-11-19 13:34:02 -0500

    added label, password field, submit button to LoginVC

---
+++ BellyFund/FirebaseAuthService.swift
@@ -0,0 +1,9 @@
+//
+//  FirebaseAuthService.swift
+//  BellyFund
+//
+//  Created by Alex 6.1 on 11/18/19.
+//  Copyright © 2019 aglegaspi. All rights reserved.
+//
+
+import Foundation
---
+++ BellyFund/Validations.swift
@@ -0,0 +1,9 @@
+//
+//  Validations.swift
+//  BellyFund
+//
+//  Created by Alex 6.1 on 11/18/19.
+//  Copyright © 2019 aglegaspi. All rights reserved.
+//
+
+import Foundation
