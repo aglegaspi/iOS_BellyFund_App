c71ce0503d8e1dcf3053622f9e3ce18c30206434
aglegaspi <lex83nyc@gmail.com>
2019-12-16 19:38:21 -0500

    set up collection view for FeedVC

---
+++ BellyFund/Controllers/User/FeedCell.swift
@@ -0,0 +1,39 @@
+//
+//  FeedCell.swift
+//  BellyFund
+//
+//  Created by Alex 6.1 on 12/16/19.
+//  Copyright © 2019 aglegaspi. All rights reserved.
+//
+
+import UIKit
+
+class FeedCell: UICollectionViewCell {
+        
+        var postImage: UIImageView = {
+            let image = UIImageView()
+            image.contentMode = .scaleAspectFit
+            return image
+        }()
+        
+        override init(frame: CGRect) {
+            super.init(frame: frame)
+            setImageConstraints()
+        }
+        
+        required init?(coder: NSCoder) {
+            fatalError("init(coder:) has not been implemented")
+        }
+        
+        func setImageConstraints() {
+            contentView.addSubview(postImage)
+            postImage.translatesAutoresizingMaskIntoConstraints = false
+            NSLayoutConstraint.activate([
+                postImage.topAnchor.constraint(equalTo: contentView.topAnchor),
+                postImage.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+                postImage.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+                postImage.widthAnchor.constraint(equalToConstant: postImage.frame.width),
+                postImage.heightAnchor.constraint(equalToConstant: contentView.frame.height - 100)])
+        }
+        
+}
--- BellyFund/Controllers/User/FeedVC.swift
+++ BellyFund/Controllers/User/FeedVC.swift
@@ -9,12 +9,125 @@
 import UIKit
 
 class FeedVC: UIViewController {
-
     
+    //MARK: PROPERTIES
+    var posts = [Item]() { didSet { collectionView.reloadData() } }
+    
+    //MARK: VIEWS
+    var projectLabel: UILabel = {
+        let label = UILabel()
+        label.text = "BellyFund"
+        label.font = UIFont.systemFont(ofSize: 40, weight: .regular)
+        label.textAlignment = .center
+        label.textColor = .black
+        return label
+    }()
+    
+    var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        let collection = UICollectionView(frame: CGRect.zero, collectionViewLayout: layout)
+        collection.backgroundColor = UIColor(red: 0.647, green: 0.286, blue: 0.198, alpha: 1.0)
+        collection.register(FeedCell.self, forCellWithReuseIdentifier: "FeedCell")
+        return collection
+    }()
+    
+    //MARK: LIFECYCLES
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor(red: 0.647, green: 0.286, blue: 0.198, alpha: 1.0)
+        setUpConstraints()
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        loadPosts()
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        loadPosts()
+    }
+    
+    //MARK: CONSTRAINTS
+    private func setUpConstraints() {
+        constrainLabel()
+        constrainCollectionView()
+    }
+    
+    private func constrainLabel() {
+        view.addSubview(projectLabel)
+        projectLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            projectLabel.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+            projectLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            projectLabel.heightAnchor.constraint(equalToConstant: 50),
+            projectLabel.widthAnchor.constraint(equalTo: view.widthAnchor)])
+    }
+    
+    private func constrainCollectionView() {
+        view.addSubview(collectionView)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: projectLabel.bottomAnchor, constant: 30),
+            collectionView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)])
+    }
+    
+    //MARK: PRIVATE FUNCTIONS
+    private func loadPosts() {
+//        FirestoreService.manager.getAllPosts(sortingCriteria: nil) { (result) in
+//            switch result {
+//            case .failure(let error):
+//                self.present(ShowAlert.showAlert(with: "Error", and: "Could not load posts: \(error)"), animated: true, completion: nil)
+//            case .success(let postsFromFirebase):
+//                self.posts = postsFromFirebase
+//            }
+//        }
+    }
+}
 
-
+//MARK: EXTENSION
+extension FeedVC: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return posts.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let post = posts[indexPath.row]
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "FeedCell", for: indexPath) as? FeedCell else { return UICollectionViewCell() }
+//        let imageURL = post.photoUrl ?? ""
+//
+//
+//        if let image = ImageHelper.shared.image(forKey: imageURL as NSString) {
+//            cell.postImage.image = image
+//        } else {
+//            ImageHelper.shared.getImage(urlStr: imageURL) { (result) in
+//                DispatchQueue.main.async {
+//                    switch result {
+//                    case .failure(let error): print(error)
+//                    case .success(let image): cell.postImage.image = image
+//                    }
+//                }
+//            }
+//        }
+        
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width - 10, height: 500)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 3
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+//        let selectedPost = posts[indexPath.row]
+//        let detailVC = PhotoDetailVC()
+//        detailVC.post = selectedPost
+//        present(detailVC, animated: true, completion: nil)
+    }
+    
 }
