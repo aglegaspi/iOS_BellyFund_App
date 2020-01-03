dc05d661b8799289da8e65a37a9296d77de3d2b2
aglegaspi <lex83nyc@gmail.com>
2020-01-03 11:30:04 -0500

    organize files and add new files

---
+++ BellyFund/Networking/AppError.swift
@@ -0,0 +1,9 @@
+//
+//  AppError.swift
+//  BellyFund
+//
+//  Created by Alex 6.1 on 12/17/19.
+//  Copyright © 2019 aglegaspi. All rights reserved.
+//
+
+import Foundation
---
+++ BellyFund/Networking/ImageHelper.swift
@@ -0,0 +1,10 @@
+//
+//  ImageHelper.swift
+//  BellyFund
+//
+//  Created by Alex 6.1 on 12/17/19.
+//  Copyright © 2019 aglegaspi. All rights reserved.
+//
+
+import Foundation
+
